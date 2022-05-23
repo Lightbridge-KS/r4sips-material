cf5fceef711be50703b51a5672f9e233eb8653e2
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-05-23 09:57:28 +0700

    add `01-hello.R`

---
+++ 01-hello.R
@@ -0,0 +1,27 @@
+print("Hello World")
+
+?print
+
+"Hello World"
+
+# This is a comment
+
+# R as a Calculator -------------------------------------------------------
+
+# Plus
+print(1 + 1)
+1 + 1
+# Minus
+2 - 1
+# Times
+3 * 2
+# Divide
+4 / 2
+
+# Integer division
+7 %/% 2
+# Modulus (remainder)
+7 %% 2
+
+
+
