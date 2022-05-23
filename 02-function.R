3f696c73a677fcf7c395ed8b1c8403f00f6b434a
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-05-23 22:43:08 +0700

    add `02-function.R`

---
+++ 02-function.R
@@ -0,0 +1,20 @@
+
+# Write Function ----------------------------------------------------------------
+
+
+hello <- function(){
+
+  print("Hello, World")
+
+}
+
+hello() # Call a function
+
+hello # Show source code
+
+
+plus_one <- function(x) {
+  x + 1
+}
+
+plus_one(2)
