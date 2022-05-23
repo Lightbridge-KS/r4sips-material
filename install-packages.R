b4037013b4301baa9344dcd085345f84b60badee
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-05-23 21:45:01 +0700

    add ways to install package

---
+++ install-packages.R
@@ -0,0 +1,14 @@
+### Install R package
+
+if(FALSE){
+#' Install package by `install.packages("PkgName")`
+
+
+install.packages("rmarkdown")
+
+#' Alternative way (quicker) using `{pak}`
+
+install.packages("pak")
+pak::pkg_install("rmarkdown")
+
+}
