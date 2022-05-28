24cb2c021c4dc4eebbb6e4d7a1dce78ecca7735e
Kittipos <ki11ip0.s.a.s@gmail.com>
2022-05-28 14:53:46 +0700

    add `map-calc.R` exercise

--- exercise/map-calc.R
+++ exercise/map-calc.R
@@ -5,3 +5,20 @@
 #' calculate mean arterial blood pressure (MAP)
 #' from systolic and diastolic blood pressure.
 
+bp_sys <- c(120, 180, 90)
+bp_dias <- c(80, 90, 60)
+
+# Calculate MAP
+
+bp_map <- (2/3) * bp_dias + (1/3) * bp_sys
+bp_map <- round(bp_map, digits = 1)
+
+print(bp_map)
+
+# Print output to console
+# cat("Mean arterial blood pressure =", bp_map)
+
+
+
+
+
